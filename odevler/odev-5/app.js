2b4c4916f9456ee61ca93b6377b65f5bb7e2368c
Varol Maksutoğlu <waroi@pisilinux.org>
2020-07-25 21:19:49 +0300

    Urun ekleme uygulaması ve ödevler eklendi

---
+++ odevler/odev-5/app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    array: ['Varol', 'Özge', 'Elif', 'Deniz'],
+    customObject: {
+      title: 'Game of Thrones',
+      author: 'George R.R',
+      books: '7'
+    },
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
---
+++ urun-ekleme/src/components/Progress.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="progress">
+      <div class="progress-bar bg-info" role="progressbar" :style="{width: progress+'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+        {{currentItem}}/10
+      </div>
+    </div>
+</template>
+
+<script>
+import {eventBus} from '../main';
+export default {
+  data() {
+    return{
+      progress:0,
+      currentItem:0,
+    }
+  },
+  created() {
+    eventBus.$on("progressBarUpdated",(productListCount) => {
+      this.currentItem = productListCount;
+      this.progress = productListCount * 10;
+    })
+  }
+
+}
+</script>
