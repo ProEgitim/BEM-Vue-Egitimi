a92e2a33fa28f35d6a25029dd59f0085534a9d67
Varol Maksutoğlu <waroi@pisilinux.org>
2020-08-16 22:19:19 +0300

    animation2 yeni örnek eklendi

---
+++ animation2/src/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="alert alert-danger">
+    <strong> Bu Post Componenti</strong>
+  </div>
+</template>
---
+++ animation2/src/components/Post.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="alert alert-info">
+    <strong> Bu Post Componenti</strong>
+  </div>
+</template>
